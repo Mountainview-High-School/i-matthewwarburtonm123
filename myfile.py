3d4f03a1b086527e7533ef71585068a25f8427ed
Matthew wangburton <126116212+matthewwarburtonm123@users.noreply.github.com>
2026-02-17 13:20:37 +1300

    yes

---
+++ myfile.py
@@ -0,0 +1,12 @@
+print("wlcome to the fluffy hair test")
+x=input("how old are you")
+if x < 10:
+    print("0% fluffy hair")
+elif x > 9:
+    print("40% fluffy hair, nice!")
+elif x < 16:
+    print("100% fuffy hair, great job diddy blud")
+elif x > 15:
+    print("40% fuffy hair, your falling of")
+elif x > 18:
+    print("")
