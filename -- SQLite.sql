3c092ee597f501b14bd2bb06766e07ea43e74d8b
Matthew wangburton <126116212+matthewwarburtonm123@users.noreply.github.com>
2026-03-16 12:18:14 +1300

    added some more files

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -17,4 +17,14 @@ Select * from class WHERE NOT class_name = '44';
 --5
 SELECT * from class WHERE class_name = '44' OR class_name = '43';
 --6 
-SELECT * FROM class WHERE class_name
+SELECT * FROM class WHERE class_name 
+
+
+
+
+
+
+
+
+
+
